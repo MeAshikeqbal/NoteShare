f6125054068779d60be4f89dd9b55566e1309040
Ashik Eqbal <ashikeqbal30@gmail.com>
2024-01-18 15:42:10 +0530

    Refactor search page to use categories component

--- app/courses/(routes)/search/page.tsx
+++ app/courses/(routes)/search/page.tsx
@@ -1,9 +1,21 @@
-const search = () => {
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
+
+const AllCoursesPage = async () => {
+    const categories = await db.catagory.findMany({
+        orderBy:{
+            name:"asc"
+        }
+    })
     return ( 
-        <div>
-            <h1>all courses</h1>
+        <div
+        className="p-6"
+        >
+            <Categories
+            items={categories}
+            />
         </div>
      );
 }
  
-export default search;
+export default AllCoursesPage;
